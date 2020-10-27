800afe00b891c9db6fc7890300ed7f72fb7637b9
Natasha <natu.rios@hotmail.com>
2020-10-26 21:49:24 -0300

    next

--- Next/blog/pages/articulo/[id].js
+++ Next/blog/pages/articulo/[id].js
@@ -13,16 +13,34 @@ export default function DynamicRoute({ title, tags, content }) {
     </div>
     )
   }
-  
-  export async function getServerSideProps({ params }) {
-      const { id } = params;
-    const data = await fetch(` https://dev.to/api/articles/${id}`);
+
+  export async function getStaticPaths() {
+    const data = await fetch("https://dev.to/api/articles?tag=javascript&top=1");
     const json = await data.json();
+  
+    // const paths = json.map(path => `/articulo/${path.id}`)
+  
+    const paths = json.map((path) => {
+      return `/articulo/${path.id}`;
+    });
+  
+    return {
+      paths: paths,
+      fallback: false
+    };
+  }
+  
+  export async function getStaticProps({ params }) {
+    const { id } = params;
+    const data = await fetch(`https://dev.to/api/articles/${id}`);
+    const json = await data.json();
+  
     return {
       props: {
         title: json.title,
         tags: json.tags,
-        content: json.body_html
-      }
+        content: json.body_markdown
+      },
+      revalidate: 3600
     };
   }
